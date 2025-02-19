d181981a294d2f64beb5593cdc503e2d8eea3fd3
Giulio <giuliopanza2000@gmail.com>
2025-02-19 22:06:27 +0100

    create header

--- src/componets/Hedear.jsx
+++ src/componets/Hedear.jsx
@@ -1,7 +1,27 @@
 function Header () {
     
     return (
-    <header></header>
+    <header>
+        <div className="container">
+            <figure>
+                <img src="src/assets/img/dc-logo.png" alt="" />
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTER</a></li>
+                    <li className="active"><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 )
     
 }
