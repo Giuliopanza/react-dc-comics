62bd70c66e2c10caa05fe1133cbd35c4893fb0b9
Giulio <giuliopanza2000@gmail.com>
2025-02-20 01:58:57 +0100

    Create Footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,7 +1,59 @@
 function Footer () {
     
     return (
-    <footer></footer>
+    <footer>
+        <section id="foottop">
+            <div className="container bg-logo">
+                <div className="container-50">
+                <div>
+                    <ul className="ul-footer">
+                        <li><h1>DC COMICS</h1></li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul className="ul-footer">
+                        <li><h1>SHOP</h1></li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectible</li>
+                    </ul>
+                </div>
+                <ul className="ul-footer">
+                    <li><h1>DC</h1></li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                </ul>
+                <ul className="ul-footer">
+                    <li><h1>SITES</h1></li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                    <li>Hello World</li>
+                </ul>
+                </div>
+            </div>
+        </section>
+        <section id="fotbottom">
+        <div className="container">
+            <button>SING-UP NOW!</button>
+        </div>
+        </section>
+
+    </footer>
 )
     
 }
